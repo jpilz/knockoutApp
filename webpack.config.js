b50ab0e8f20454ee645de7fc0fc0e167015f4a3e
Jeff Pilz <jpilz@develup.com>
2019-01-15 16:13:06 -0500

    Webpack 4 and Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,85 @@
-var webpack = require("webpack");
 const path = require('path');
+const webpack = require("webpack");
+const fs = require('fs');
 
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+
+    mode: 'development',
     entry: './src/app.js',
+    devtool: 'inline-source-map',
+    watch: true,
+
     output: {
-        path: path.resolve(__dirname,'lib'),
         filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'lib')
     },
 
+    devServer: {
+      watchContentBase: true,
+      disableHostCheck: true,
+      publicPath: '/lib/',
+      host: '127.0.0.1',
+      port: 8080,
+      contentBase: __dirname,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      watchOptions: {
+        ignored: /node_modules/
+      }
+    },
+    
+    target: 'web',
+    node: {
+      fs: 'empty'
+    },
+    
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader?presets[]=es2015'
+      rules: [{
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
         },
         {
-            test: /\.html$/,
-            loader: 'html',
-            query: {
-                minimize: false
+          test: /\.html$/,
+          use: [{
+            loader: 'html-loader',
+            options: {
+              minimize: true
             }
-        },{
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
-        }
-        ,{
-            test: /\.(woff|woff2|eot|ttf|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loaders: 'url-loader?hash=sha512&digest=hex&name=[hash].[ext]'
+          }]
         },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                loaders: [
-                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
-            }
-        ]
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }, {
+          test: /\.(woff|woff2|eot|ttf|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: [{
+            loader: "url-loader?limit=50000&name=fonts/[name].[ext]"
+          }]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'file-loader'
+        }
+      ]
     },
 
-    plugins: !debug ?
-    [
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                compress: {
-                    drop_console: true
-                    
-                },
-                output: {
-                    comments: false
-                }
-            }
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            ko: "knockout"
-        })
-    ] 
-    :
-    [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            ko: "knockout"
-        })
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        ko: "knockout"
+      })
     ]
 }
